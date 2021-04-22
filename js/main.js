9f74c1ea7b9259cec6b7ab250876343917280658
rud-andreos <rud.andreos@gmail.com>
2021-04-22 16:59:51 -0300

    2021_04_22_primeira_aula

---
+++ js/main.js
@@ -0,0 +1,13 @@
+
+var nome = " Rudge Andreos";
+var idade = 39;
+var idade2 = 10;
+var frase = "Japão é o melhor time do mundo";
+//alert("Bem vindo" + nome);
+//alert(idade + idade2);
+console.log(nome);
+console.log(idade + idade2);
+/*console.log(frase.replace("Japão", "Brasil"));
+//console.log(frase.toUpperCase());
+//console.log(frase.toLowerCase());*/
+alert(frase.replace("Japão", "Brasil"));
