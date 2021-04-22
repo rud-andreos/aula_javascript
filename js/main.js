8a0f1fe84618c00b16239d3d57a7e0a99f13f580
rud-andreos <rud.andreos@gmail.com>
2021-04-22 17:34:03 -0300

    2021_04_22_segunda_aula

--- js/main.js
+++ js/main.js
@@ -1,4 +1,61 @@
+/*
+var d = new Date();
+alert(d.getMonth());
+alert(d.getDay());
+alert(d.getHours());
+alert(d.getMinutes());
+alert(d.getSeconds());
+//alert(d);
+*/
 
+/*
+var count;
+for(count=0; count <=5; count++){
+    alert(count);
+};
+*/
+
+/*
+var count = 0;
+while (count < 5){
+    console.log(count);
+    alert(count);
+    //count = count + 1;
+    count++;
+};
+*/
+
+/*var idade = prompt("Qual sua idade?");
+//var idade = 18;
+if(idade >= 18){
+    alert("maior de idade");
+}
+else{
+    alert("menor de idade");
+};
+*/
+
+/*var frutas = [{nome:"maça", cor:"vermelha"}, {nome:"uva", cor:"roxa"}]
+console.log(frutas);
+alert(frutas[1].cor);
+*/
+
+/*
+var fruta = {nome:"maça", cor:"vermelha"}
+console.log(fruta.nome);
+alert(fruta.cor);
+/*
+
+//var lista = ["maça", "pêra", "laranja"];
+//lista.push("uva");
+//lista.pop();
+//console.log(lista[1]); 
+//console.log(lista);
+//console.log(lista.toString());
+//console.log(lista.join(" | "));
+
+
+/*
 var nome = " Rudge Andreos";
 var idade = 39;
 var idade2 = 10;
@@ -7,7 +64,8 @@ var frase = "Japão é o melhor time do mundo";
 //alert(idade + idade2);
 console.log(nome);
 console.log(idade + idade2);
-/*console.log(frase.replace("Japão", "Brasil"));
+//console.log(frase.replace("Japão", "Brasil"));
 //console.log(frase.toUpperCase());
-//console.log(frase.toLowerCase());*/
+//console.log(frase.toLowerCase());
 alert(frase.replace("Japão", "Brasil"));
+*/
