9ebb140626625427810889d2a905cbd8c80e5f8b
rud-andreos <rud.andreos@gmail.com>
2021-04-22 19:07:31 -0300

    2021_04_22_js_básico_vfinal

--- js/main.js
+++ js/main.js
@@ -1,3 +1,47 @@
+function clicou(){
+    document.getElementById("agradecimento").innerHTML = "Obrigado por clicar";
+    //console.log(document.getElementById("agradecimento"));
+    //alert("Obrigado por clicar");
+}
+
+function redirecionar(){
+    //window.open("https://globallabs.academy/");
+    window.location.href = "https://globallabs.academy/";
+}
+
+function trocar(){
+    document.getElementById("mousemove").innerHTML = "Obrigado por passar o mouse";
+    //alert("trocar texto");
+}
+
+function voltar(){
+    document.getElementById("mousemove").innerHTML = "Passe o mouse aqui";
+}
+
+/*
+function soma(n1, n2){
+    return n1 + n2;
+}
+*/
+
+/*
+function validaIdade(idade){
+    var validar;
+    if (idade >= 18){
+        validar = true
+    }
+    else{
+        validar = false
+    }
+    return validar;
+}
+var idade = prompt("Qual sua idade?");
+console.log(validaIdade(idade));
+*/
+
+//alert(soma(5, 10));
+
+
 /*
 var d = new Date();
 alert(d.getMonth());
